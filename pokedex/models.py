92b560228553a378f45e9c2f3fec137bd73c1a76
akassab <andrewkassab@live.com>
2021-06-24 11:04:16 -0700

    removed pycache and made weak_to / resistant_to non symetrical

--- pokedex/models.py
+++ pokedex/models.py
@@ -6,7 +6,7 @@ class Type(models.Model):
 	name = models.CharField(max_length=15, primary_key=True)
 	color = ColorField()
 	weak_to = models.ManyToManyField('self', blank=True, symmetrical=False)
-	resistant_to = models.ManyToManyField('self', blank=True, symmetrical=False)
+	resistant_to = models.ManyToManyField('self', related_name='resistant', blank=True, symmetrical=False)
 
 	def __str__(self):
 		return self.name
