2ee1b85634c3d83bd02bcce951a38e570ecbd666
akassab <andrewkassab@live.com>
2021-06-28 18:36:44 -0700

    pokedex index now shows list of pokemon

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Pokemon
 
 
 # TODO:
 def pokedex(request):
-	return render(request, 'pokedex.html', {})
+	pokemon = Pokemon.objects.all()
+	context = {
+		"pokemon": pokemon 
+	}
+	print(pokemon)
+	return render(request, 'pokedex.html', context)
 
 
 # TODO:
