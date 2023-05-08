daa00f3370ff2d0d34f22296674e806f98b8486d
andrewkassab <andrewkassab@live.com>
2023-05-08 15:00:35 -0700

    updated template to display weaknesses and an image of the pokemon

--- src/main/java/andrewkassab/pokedex/controller/PokemonController.java
+++ src/main/java/andrewkassab/pokedex/controller/PokemonController.java
@@ -17,8 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PokemonController {
 
     private static final String POKEMON_PATH = "/pokemon";
-
     private static final String POKEMON_PATH_ID = POKEMON_PATH + "/{pokemonId}";
+
+    private final String imgSourceUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
 
     private final PokemonService pokemonService;
 
@@ -26,6 +27,7 @@ public class PokemonController {
     public String viewPokemon(@PathVariable("pokemonId") Integer pokemonId, Model model) {
         Pokemon pokemon = pokemonService.getPokemonById(pokemonId).orElseThrow(NotFoundException::new);
         model.addAttribute("pokemon", pokemon);
+        model.addAttribute("imgSourceUrl", imgSourceUrl);
         return "pokemon-details";
     }
 
--- src/main/java/andrewkassab/pokedex/models/Type.java
+++ src/main/java/andrewkassab/pokedex/models/Type.java
@@ -1,27 +1,38 @@
 package andrewkassab.pokedex.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
 
 public enum Type {
 
-    NORMAL,
-    FIRE,
-    WATER,
-    ELECTRIC,
-    GRASS,
-    ICE,
-    FIGHTING,
-    POISON,
-    GROUND,
-    FLYING,
-    PSYCHIC,
-    BUG,
-    ROCK,
-    GHOST,
-    DRAGON,
-    DARK,
-    STEEL,
-    FAIRY;
+    NORMAL("Normal", "#A8A878"),
+    FIRE("Fire", "#F08030"),
+    WATER("Water", "#6890F0"),
+    GRASS("Grass", "#78C850"),
+    ELECTRIC("Electric", "#F8D030"),
+    ICE("Ice", "#98D8D8"),
+    FIGHTING("Fighting", "#C03028"),
+    POISON("Poison", "#A040A0"),
+    GROUND("Ground", "#E0C068"),
+    FLYING("Flying", "#A890F0"),
+    PSYCHIC("Psychic", "#F85888"),
+    BUG("Bug", "#A8B820"),
+    ROCK("Rock", "#B8A038"),
+    GHOST("Ghost", "#705898"),
+    DRAGON("Dragon", "#7038F8"),
+    DARK("Dark", "#705848"),
+    STEEL("Steel", "#B8B8D0"),
+    FAIRY("Fairy", "#EE99AC");
+
+    private final String name;
+
+    @Getter
+    private final String color;
+
+    Type(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
 
     @JsonCreator
     public static Type fromValue(String value) {
@@ -40,6 +51,11 @@ public enum Type {
             }
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 
 }
